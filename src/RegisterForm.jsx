11e8d17dc79fe5b6c0afc7788ff203bb630c86b8
Ashures <ashuresyt@gmail.com>
2023-09-30 20:52:09 +1300

    Created RegisterForm component to register user

---
+++ src/RegisterForm.jsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useState } from "react";
+
+export default function RegisterForm() {
+    const [username, setUsername] = useState("");
+    const [token, setToken] = useState("");
+
+    const checkRegister = async () => {
+        const error = document.getElementById("register-error");
+        const options = {
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json",
+            },
+            "body": JSON.stringify({
+                symbol: username,
+                faction: "COSMIC",
+            }),
+        };
+        
+        const res = await fetch("https://api.spacetraders.io/v2/register", options);
+
+        if (!res.ok) {
+            error.style.display = "block";
+            return;
+        }
+        
+        const data = await res.json();
+        error.style.display = "none";
+        
+        setToken(await data.data.token);
+        
+        const success = document.getElementById("register-success");
+        success.style.display = "block";
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (username.length === 0) return;
+
+        checkRegister();
+    };
+
+    return (
+        <form className="entry-form" id="register-form" onSubmit={handleSubmit} method="POST">
+            <div className="login-row" id="register-error">
+                <span>
+                    Username taken!
+                </span>
+            </div>
+            <div className="login-row">
+                <input type="text" name="username" id="register-username" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
+            </div>
+            <div className="login-row">
+                <input type="text" name="token" id="register-token" placeholder="Token" value={token} readOnly />
+            </div>
+            <div className="login-row login-btn">
+                <button>Register</button>
+            </div>
+            <div className="login-row" id="register-success">
+                <span>
+                    Success! Proceed to login.
+                </span>
+            </div>
+        </form>
+    );
+}
