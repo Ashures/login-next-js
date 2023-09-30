7237b2e0e28ed1e775c3244806f404f3375c3ac8
Ashures <ashuresyt@gmail.com>
2023-09-30 19:44:40 +1300

    Added loading text whilst the agent hasn't loaded

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react"
+import AgentInfo from "@/AgentInfo";
 
 export default function HomePage() {
     const [token, setToken] = useState(() => {
@@ -8,13 +9,16 @@ export default function HomePage() {
         const cookieList = cookies.split(";");
 
         let user;
-        cookieList.forEach(cookie => { if (cookie.includes("token=")) user = cookie.slice(cookie.indexOf("=") + 1) });
+        cookieList.forEach(cookie => { if (cookie.includes("token=")) user = cookie.slice(cookie.indexOf("=") + 1) });;
 
         return user;
     });
     const [agent, setAgent] = useState("");
+    const [agentInfo, setAgentInfo] = useState({});
 
     useEffect(() => {
+        if (!token) goToLogin();
+
         const options = {
             "headers": {
                 "Content-Type": "application/json",
@@ -24,12 +28,31 @@ export default function HomePage() {
 
         fetch("https://api.spacetraders.io/v2/my/agent", options)
         .then(res => res.json())
-        .then(data => setAgent(data.data.symbol));
+        .then(data => {
+            setAgent(data.data.symbol);
+            setAgentInfo(data.data);
+        });
     }, [token]);
 
+    const goToLogin = () => {
+        document.cookie = "token= ; expires = Thu, 01 Jan 1970 00:00:00 GMT"
+
+        window.location.href = "/";
+    };
+
     return (
-        <>
-            <h1>Welcome, {agent}</h1>
-        </>
-    )
+        <div className="home-page">
+            {(agent.length !== 0) 
+            ? 
+                <>
+                    <h1>Welcome, {agent}</h1>
+                    <AgentInfo info={agentInfo} />
+                </>
+            :
+                <>
+                    <h1>Loading...</h1>
+                </>
+            }
+        </div>
+    );
 };
